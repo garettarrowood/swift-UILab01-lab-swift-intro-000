c9bb41a39fc4a2b131f6bd0e6a80d1407f05f154
Garett Arrowood <garettarrowood@gmail.com>
2017-06-04 12:59:39 -0400

    That was it?

--- LightBulb/ViewController.swift
+++ LightBulb/ViewController.swift
@@ -1,35 +1,26 @@
-//
-//  ViewController.swift
-//  LightBulb
-//
-//  Created by Jim Campagno on 9/17/16.
-//  Copyright © 2016 Flatiron School. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-    
     @IBOutlet weak var lightBulb: UIImageView!
-    
-    
+
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        
         lightBulb.backgroundColor = UIColor.blue
-        
     }
-    
-    
+
     @IBAction func colorSelected(_ sender: UISegmentedControl) {
-        
-        print(sender.selectedSegmentIndex)
-        
+        switch sender.selectedSegmentIndex {
+        case 0:
+             lightBulb.backgroundColor = UIColor.red
+        case 1:
+            lightBulb.backgroundColor = UIColor.yellow
+        case 2:
+            lightBulb.backgroundColor = UIColor.blue
+        case 3:
+            lightBulb.backgroundColor = UIColor.green
+        default:
+            print("nothing")
+        }
     }
-    
-    
-
-   
-
 }
